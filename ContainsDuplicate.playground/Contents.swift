f1cbf53438ca095ee2e2fc2380fbb0377b3e7f2c
Mayur P <64838833+zephyrwala@users.noreply.github.com>
2024-01-08 11:40:53 +0530

    contains duplicate updated

--- ContainsDuplicate.playground/Contents.swift
+++ ContainsDuplicate.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+//Input: nums = [1,2,3,1]
+//Output: true
+
+
+func containsDuplicate(_ nums: [Int]) -> Bool {
+    var numSet = Set<Int>()
+    
+    for num in nums {
+        if numSet.contains(num) {
+            return true
+        } else {
+            numSet.insert(num)
+        }
+    }
+    
+    return false
+}
+
+
+let obj = containsDuplicate([1,8,4,3])
