9e9551f1df1097c9122be6a8a33832a966fa3c46
Mayur P <64838833+zephyrwala@users.noreply.github.com>
2024-01-09 09:21:38 +0530

    Hash Set method

--- ContainsDuplicate.playground/Pages/Dictionary_HashMaps.xcplaygroundpage/Contents.swift
+++ ContainsDuplicate.playground/Pages/Dictionary_HashMaps.xcplaygroundpage/Contents.swift
@@ -2,19 +2,68 @@
 
 import Foundation
 
-let obj = containsDuplicate([1,8,4,3])
+//let obj = containsDuplicate([1,8,4,3])
 
 
 //creating empty hash
 
+class Solution {
+    
+    
+    func duplicateCheck(nums: [Int]) -> Bool {
+        var myHashSet = Set<Int>()
+        for eachNum in nums {
+            if myHashSet.contains(eachNum){
+                //if number already exist > return true for duplicate found
+                print("Duplicate Found: \(eachNum)")
+                return true
+                //check
+                
+                
+            } else {
+                //add to the set hash if there is no number
+                myHashSet.insert(eachNum)
+               
+                
+                
+            }
+        }
+        return false 
+        
+    }
+}
 
-var myDicts : [String: Bool] = [:]
-//adding key:values
-myDicts["Raju"] = true
-myDicts["Babu"] = false
+let myObj = Solution()
+myObj.duplicateCheck(nums: [1,2,4,6,6])
 
-print(myDicts)
-//removing key: values
-myDicts.removeValue(forKey: "Raju")
 
-print(myDicts)
+
+
+class MySolution {
+    
+    
+    func findDuplicates(numbers: [Int]) -> Bool{
+        
+        var hashSet = Set<Int>()
+        
+        for eachNumber in numbers {
+            
+            if hashSet.contains(eachNumber) {
+                print("We Got a duplicate in hashSet : \(eachNumber)")
+                return true
+            } else {
+                hashSet.insert(eachNumber)
+            }
+            
+            
+        }
+        return false
+        
+    }
+    
+    
+}
+
+
+let myNewObject = MySolution()
+myNewObject.findDuplicates(numbers: [1,3,5,6,7,8,8])
